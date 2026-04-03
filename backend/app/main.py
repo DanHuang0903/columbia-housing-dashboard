05e7d07fc57248d250b9bf08ee3ebdfff3cdbc39
Maggiexuiscute <your.maggiexuiscute@gmail.com>
2026-04-03 12:36:32 -0500

    add CORS origns

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://columbia-housing.netlify.app"],
+    allow_origins = [
+        "https://columbia-housing.netlify.app",
+        "http://localhost:5173",
+        "http://localhost:3000",
+        ],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
